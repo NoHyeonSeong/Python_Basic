d0177916cbe7ced02d557b2be979401ee92efcf4
NoHyeonSeong <143773684+NoHyeonSeong@users.noreply.github.com>
2023-12-01 11:34:04 +0900

    Add files via upload

--- lecture/python_11_scheduler.py
+++ lecture/python_11_scheduler.py
@@ -1,27 +1,21 @@
-# 스케줄러(Scheduler)
-# - 정해진 일정에 맞춰서 프로그램을 동작!
-# 예) 12시간에 한번씩
-#     5분마다
-#     특정일에만(크리스마스)
-
-# 스케줄러 + 프로그램 → 완성 → 서버(동작)
+# 스케줄러
+#  - 정해진 시간에 동작하도록 프로그램을 스케줄링
+#  예) 5초마다 한번씩 동작
+#      특정일(크리스마스)에만 동작
+#      하루에 한 번 동작
 
 # apscheduler
-# 1. blocking
-#   - 스케줄러 + 프로그램만 동작
-# 2. background
-#   - 동작은 하되 뒤에서 조용히!
-
+#  1.blocking
+#  2.background
 
 from apscheduler.schedulers.blocking import BlockingScheduler
 from datetime import datetime
+def print_now():
+    print(datetime.now())
 
-def print_today():
-    print(datetime.now())   # 현재시간 출력
-
-
-# 1. 스케줄러 생성
+# 1.스케줄러 생성
 scheduler = BlockingScheduler()
-
-scheduler.add_job(print_today, "interval", seconds=5)
+# 2.스케줄러 등록(일, 주기, 5초)
+scheduler.add_job(print_now, "interval", seconds=5)
+# 3.스케줄러 실행
 scheduler.start()
